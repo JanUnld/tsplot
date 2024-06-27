fadc58c55061c9bbd87cefa5af62236a24c80fa9
Jan Unnold <j.unnold@gmx.de>
2024-06-27 22:47:57 +0200

    feat: experimental nunjucks templating renderer

--- packages/tsplot/src/index.ts
+++ packages/tsplot/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/core';
 export * from './lib/render';
+export * from './lib/render2';
 export * from './lib/filter';
--- packages/tsplot/src/lib/index.ts
+++ packages/tsplot/src/lib/index.ts
@@ -1,3 +1,4 @@
 export * from './core';
 export * from './render';
+export * from './render2';
 export * from './filter';
